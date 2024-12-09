a9ba5bf6eda1203f11558b532736badd1557d500
qbning <88238811+qbning@users.noreply.github.com>
2024-12-09 18:22:25 +0800

    Update InfoLine.vue
    没有指明作者时下方信息栏不再显示unknown author，而是直接不显示

--- components/InfoLine.vue
+++ components/InfoLine.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="absolute bottom-0 left-0 right-0 flex text-center text-white">
-		<div class="flex-1 p-1" style="background:#191959">
-			{{ $slidev.configs.author || "Unknown author" }}
+		<div v-if="$slidev.configs.author" class="flex-1 p-1" style="background:#191959">
+			{{ $slidev.configs.author }}
 		</div>
 		<div class="flex-1 p-1" style="background:#262686">
 			{{ $slidev.configs.title || "Unknown title" }}
